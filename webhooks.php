b6d07bd58c949c3570401273644204618ec95bc4
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-12 01:38:15 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -25,6 +25,8 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
+			
+			$id = $arrayJson['events'][0]['source']['userId'];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
