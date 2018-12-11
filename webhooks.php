5999897f0ec3116268af47c970092de8d047e3d9
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-12 01:33:15 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -45,6 +45,16 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
+			
+			if($message == "สวัสดี"){
+     			$arrayPostData['to'] = $id;
+      			$arrayPostData['messages'][0]['type'] = "text";
+      			$arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+      			$arrayPostData['messages'][1]['type'] = "sticker";
+      			$arrayPostData['messages'][1]['packageId'] = "2";
+      			$arrayPostData['messages'][1]['stickerId'] = "34";
+      			pushMsg($arrayHeader,$arrayPostData);
+   }
 		}
 	}
 }
