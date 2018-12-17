99923823b0e7d6d26d130619de9fd070dea05416
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-17 15:18:08 +0700

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	mysql_connect("heroku_b577b61b9273cc5","b1ab7271b69167","f300f19a");
+	mysql_connect("us-cdbr-iron-east-01.cleardb.net","b1ab7271b69167","f300f19a");
 	mysql_select_db("heroku_b577b61b9273cc5");
 	$strSQL = "INSERT INTO `heroku_b577b61b9273cc5` (LineID,Message) VALUES ('".$_POST["txtlineid"]."' , '".$_POST["txtmessage"]."')";
 	$objQuery = mysql_query($strSQL);
