821d7c377ddd1441ef82213feb8677e253eaf0b6
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-17 16:42:25 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "us-cdbr-iron-east-01.cleardb.net";
-$username = "b1ab7271b69167";
-$password = "f300f19a";
+$username = "b1ab7271b69167aa";
+$password = "f300f19aaa";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
