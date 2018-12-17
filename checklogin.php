1bb98f7df4633ed2d676c017a59567158c6e62ef
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-17 17:45:20 +0700

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -1,8 +1,12 @@
 <?php
+
+require "vendor/autoload.php
+
 $servername = "us-cdbr-iron-east-01.cleardb.net";
 $username = "b1ab7271b69167";
 $password = "f300f19a";
 $dbname = "heroku_b577b61b9273cc5";
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -19,9 +23,6 @@ if ($conn->query($sql) === TRUE) {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-$conn->close();
-require "vendor/autoload.php
-
 $access_token = '6Fkcia04Z6b5eNyPFvCTM98VKSofDCm3zr8tX1XrbPSdMBCCCDhNhxXiJTP3wIjZT2Jj+EZ0Cr58AFhw1ZcSus9kH/gpj+N5N3kQZzkOJb3aM6wm4R5oWjt4o6kJsqsjCPDldpOY4L6/+QRmzgqYbwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'cb35ef400aeeb1531a9c836e5d3e72ed';
 
@@ -32,4 +33,5 @@ $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('".$_P
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
+$conn->close();
 ?>
