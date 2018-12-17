0ac1e347c9714fb10a4ba4f25fd86f7f4d24751d
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-17 15:34:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
 
-$url = parse_url(getenv("us-cdbr-iron-east-01.cleardb.net"));
-
-$server = $url["host"];
-$username = $url["b1ab7271b69167"];
-$password = $url["f300f19a"];
-$db = substr($url["heroku_b577b61b9273cc5"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
-
-echo "TEST";
+echo "TEST HOST SERVER";
 
 ?>
