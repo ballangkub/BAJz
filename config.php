02a0d1a0dcb960e74d6fe412a1d070433bfc450e
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-17 16:39:30 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,11 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$url = parse_url(getenv("us-cdbr-iron-east-01.cleardb.net"));
-
-$server = $url["us-cdbr-iron-east-01.cleardb.net"];
-$username = $url["b1ab7271b69167"];
-$password = $url["f300f19"];
-$db = substr($url["heroku_b577b61b9273cc5"], 1);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
-if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-} else {
-      echo "Connected successfully";
-}
-
-mysqli_close($conn);
 
 ?>
