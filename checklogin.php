de85fc32d6c2ac3ccc7d1458ed0ae17f3e45f8c4
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-17 16:09:41 +0700

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -4,7 +4,7 @@
 	$server = $url["us-cdbr-iron-east-01.cleardb.net"];
 	$username = $url["b1ab7271b69167"];
 	$password = $url["f300f19a"];
-	$db = substr($url["heroku_b577b61b9273cc5"], 1);
+	$db = substr($url["heroku_b577b61b9273cc51"], 1);
 
 	$conn = new mysqli($server, $username, $password, $db);
 
