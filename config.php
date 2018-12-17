44074a068f39718b96120006e49ca3080216a842
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-17 15:39:16 +0700

    Update config.php

--- config.php
+++ config.php
@@ -8,5 +8,12 @@ $password = $url["f300f19a"];
 $db = substr($url["heroku_b577b61b9273cc5"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+ 
+  echo "Connected successfully";
+ 
+mysqli_close($conn);
 
 ?>
