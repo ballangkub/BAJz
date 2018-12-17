f692c0bacec2fa44611b3e58338affbf362ed6d6
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-17 16:36:01 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $url = parse_url(getenv("us-cdbr-iron-east-01.cleardb.net"));
 
 $server = $url["us-cdbr-iron-east-01.cleardb.net"];
 $username = $url["b1ab7271b69167"];
-$password = $url["f300f19a"];
+$password = $url["f300f19"];
 $db = substr($url["heroku_b577b61b9273cc5"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
