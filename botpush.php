9c2ae1b5b8eae593f2fe127b7d85a7a44e7ac77b
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-17 17:08:31 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -1,5 +1,15 @@
 <?php
 
+$servername = "us-cdbr-iron-east-01.cleardb.net";
+$username = "b1ab7271b69167";
+$password = "f300f19a";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 
 
 require "vendor/autoload.php";
