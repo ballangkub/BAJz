d966b1ca77ae50ecb20571d63b5bace38c01b278
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-17 15:10:46 +0700

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -1,17 +1,18 @@
 <?php
 	session_start();
-	mysql_connect("127.0.0.1","root","12345678");
-	mysql_select_db("test");
-	$strSQL = "INSERT INTO linebot (LineID,Message) VALUES ('".$_POST["txtlineid"]."' , '".$_POST["txtmessage"]."')";
+	mysql_connect("heroku_b577b61b9273cc5","b1ab7271b69167","f300f19a");
+	mysql_select_db("heroku_b577b61b9273cc5");
+	$strSQL = "INSERT INTO `heroku_b577b61b9273cc5` (LineID,Message) VALUES ('".$_POST["txtlineid"]."' , '".$_POST["txtmessage"]."')";
 	$objQuery = mysql_query($strSQL);
 	if($objQuery)
 	{
 			echo "NICE LOGIN";
-			header("Location:linebotapi.php");
+			
 	}
 	else
 	{
 			echo "ERROR";
+		header("Location:linebotapi.php");
 	}
 	
 	mysql_close();
