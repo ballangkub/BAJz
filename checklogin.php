35500f02faaf5d93cff2860fba28415bf6d25761
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-17 15:51:12 +0700

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -10,8 +10,8 @@
 
 if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
-} else { echo ""Connected Successfully";
-
+} else { 
+	echo "Connected Successfully";
 }
 	$sql = "INSERT INTO details (LineID , Message) VALUES ('TEST','TEST')";
         if (mysqli_query($conn , $sql)) {
