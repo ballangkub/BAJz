e1d3b06fbb59690d34d35919a41a32a0cd482f07
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-17 17:42:34 +0700

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -20,4 +20,16 @@ if ($conn->query($sql) === TRUE) {
 }
 
 $conn->close();
+require "vendor/autoload.php
+
+$access_token = '6Fkcia04Z6b5eNyPFvCTM98VKSofDCm3zr8tX1XrbPSdMBCCCDhNhxXiJTP3wIjZT2Jj+EZ0Cr58AFhw1ZcSus9kH/gpj+N5N3kQZzkOJb3aM6wm4R5oWjt4o6kJsqsjCPDldpOY4L6/+QRmzgqYbwdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'cb35ef400aeeb1531a9c836e5d3e72ed';
+
+$pushID = '".$_POST["txtlineid"]."';
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('".$_POST["txtmessage"."']);
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
 ?>
