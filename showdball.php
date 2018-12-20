d7ba498f9950c2909ad8fc7d322d4b23beac7a8e
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-20 15:02:49 +0700

    Create showdball.php

---
+++ showdball.php
@@ -0,0 +1,29 @@
+<?php
+
+$servername = "us-cdbr-iron-east-01.cleardb.net";
+$username = "b1ab7271b69167";
+$password = "f300f19a";
+$dbname = "heroku_b577b61b9273cc5";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else { 
+    echo "Connection Success";
+}
+
+
+$sql = "SELECT LineID , Message FROM details";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "1 : " .$row["LineID"]. " - 2 : " .$row["Message"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
