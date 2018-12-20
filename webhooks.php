cae96537a0ac81508b510a718c823aa5daf740fb
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-20 14:31:30 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,5 +1,22 @@
 <?php // callback.php
 
+$servername = "us-cdbr-iron-east-01.cleardb.net";
+$username = "b1ab7271b69167";
+$password = "f300f19a";
+$dbname = "heroku_b577b61b9273cc5";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "INSERT INTO details ( LineID , Message) VALUES ('".$_POST["txtlineid"]."' , '".$_POST["txtmessage"]."')";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
