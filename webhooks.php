fc2570f9f7a7160cfd23ea906dce35d440d9c969
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-20 17:06:54 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -41,8 +41,8 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $text = $arrPostData['messages']['text'];
-  $arrPostData['messages'][0]['text'] = $text;
+  $text = $arrJson['messages']['text'];
+  $arrPostData['messages'][0]['text'] = "ระบบได้รับข้อความ ('.$text.') ของคุณแล้ว";
   //$idcode = $arrJson['events'][0]['source']['userId'];
  // $nameline = $arrJson['events'][0]['source']['displayName'];
 } else {
