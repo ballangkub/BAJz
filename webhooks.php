b00dc04957824453e1dc44910f9a506e6cdd59ba
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-20 14:46:33 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -40,12 +40,11 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
   $sql = "INSERT INTO details ( LineID ) VALUES ('".$arrJson['events'][0]['source']['userId']."')";
-if ($conn->query($sql) === TRUE) {
+} else if ($conn->query($sql) === TRUE) {
   $arrPostData['messages'][0]['text'] = "New record created successfully";
-} else {
+} else if ($conn->query($sql) === FALSE) {
     echo "Error";
-}
-}else{
+} else {
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
