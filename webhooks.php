0f3177ebd95a4a8bcc55aa0f085b9d0eb19f0003
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-20 15:15:47 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -67,9 +67,6 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['text'] = $arrJson['events'][0]['source']['userId'];
 }
 
-echo $test2;
-
-
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
