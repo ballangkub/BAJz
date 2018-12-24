9202cd3ddf928995c6d33c2dcc018d201bbbf5b6
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-25 00:01:01 +0700

    Update submitregister.php

--- submitregister.php
+++ submitregister.php
@@ -15,6 +15,12 @@ if ($conn->connect_error) {
 
 $test1 = $_POST["txtname"];
 $test2 = '1';
+$sql3 = "SELECT UserID FROM botline WHERE UserID = '$test2'";
+$result3 = $conn->query($sql3);
+if($test2 == $test2){
+    $test2 + 1;
+}
+echo $test2;
 $sql = "SELECT Name FROM botline WHERE Name = '$test1'";
 $result = $conn->query($sql);
 if ($result->num_rows >= 1) {
