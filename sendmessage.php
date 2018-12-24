26b3730aed30422a7245750ae3e59da266ba6752
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-24 22:35:59 +0700

    Update sendmessage.php

--- sendmessage.php
+++ sendmessage.php
@@ -4,14 +4,15 @@ $servername = "us-cdbr-iron-east-01.cleardb.net";
 $username = "b1ab7271b69167";
 $password = "f300f19a";
 $dbname = "heroku_b577b61b9273cc5";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 mysqli_query($conn,"SET NAMES'utf8'");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+} else { 
+    echo "Connection Success";
+}
 
 $test1 = $_POST["txtname"];
 $test2 = $_POST["txtmessage"];
