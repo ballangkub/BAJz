8e6956523f18b126fb6d2cdcd1b8f786874c1880
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-24 20:07:42 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -50,7 +50,7 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
-if($arrJson['events'][0]['message']['text'] == '$row2["Name"]'){
+if($arrJson['events'][0]['message']['text'] == $row2["Name"]){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $text = $arrJson['events'][0]['message']['text'];
