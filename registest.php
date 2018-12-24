9997fcc68bf6dea3f5acdfb24581d8edd8759ccb
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-24 23:29:48 +0700

    Create registest.php

---
+++ registest.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>TEST REGISTER</title>
+</head>
+	
+<body>	
+		<form name="form1" method="post" action="submitregister.php">
+Register TEST<br>
+<table border="1" style="width: 300px">
+<tbody>
+
+<tr>
+<td> &nbsp;Name</td>
+<td>
+<input name="txtname" type="text" id="txtname">
+</td>
+</tr>
+</tbody>
+</table>
+
+<br>
+
+<input type="submit" name="Submit" value="Submit">
+</form>
+
+</body>
+</html>
