0db7cbb51fab124caa6b163600dc2e518ced4bef
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-24 20:24:09 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -52,7 +52,8 @@ if ($result2->num_rows > 0) {
 }
 
 echo $gggg;
- 
+echo $gggg;
+
 if($arrJson['events'][0]['message']['text'] == $gggg) {
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
