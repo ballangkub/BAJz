4f39a85bdf970623364b063894da7342e3a464a2
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-24 18:28:03 +0700

    Update TEST.php

--- TEST.php
+++ TEST.php
@@ -19,7 +19,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "UserID : " .$row["Name"].;
+        echo "Text : " .$row["Name"].;
     }
 } else {
     echo "0 results";
