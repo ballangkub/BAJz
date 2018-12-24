c359366dc7eed461f50500a538494ebcacc3da4d
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-24 21:36:57 +0700

    Update sendmessage.php

--- sendmessage.php
+++ sendmessage.php
@@ -1,19 +1,19 @@
 <?php
-/*
+
 $servername = "us-cdbr-iron-east-01.cleardb.net";
 $username = "b1ab7271b69167";
 $password = "f300f19a";
 $dbname = "heroku_b577b61b9273cc5";
 
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+mysqli_query($conn,"SET NAMES'utf8'");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "INSERT INTO details ( LineID , Message) VALUES ('".$_POST["txtlineid"]."' , '".$_POST["txtmessage"]."')";
+/*$sql = "INSERT INTO details ( LineID , Message) VALUES ('".$_POST["txtlineid"]."' , '".$_POST["txtmessage"]."')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
