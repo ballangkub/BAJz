7fa9644ce1e8643fa77dd049743f17fe4cbc8b2e
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-25 00:09:50 +0700

    Update submitregister.php

--- submitregister.php
+++ submitregister.php
@@ -14,19 +14,13 @@ if ($conn->connect_error) {
 }
 
 $test1 = $_POST["txtname"];
-$test2 = '1';
-$sql3 = "SELECT UserID FROM botline WHERE UserID = '$test2'";
-$result3 = $conn->query($sql3);
-if($test2 == $test2){
-    $test2 + 1;
-}
-echo $test2;
+$max = "SELECT MAX( UserID ) FROM botline;"
 $sql = "SELECT Name FROM botline WHERE Name = '$test1'";
 $result = $conn->query($sql);
 if ($result->num_rows >= 1) {
     echo "Existing";
 } else {
-    $sql2 = "INSERT INTO botline ( Name , UserID ) VALUES ('".$test1."' , '".$test2."')";
+    $sql2 = "INSERT INTO botline ( Name , UserID ) VALUES ('".$test1."' , $max+1)";
     if($conn->query($sql2) === TRUE){
         echo "ADD SUCCESS";
     } else {
