8e884e64146455b04a445a11d951406512b02e52
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-24 21:07:28 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -44,7 +44,7 @@ $numberstu = $arrJson['events'][0]['message']['text'];
 $sql2 = "SELECT Name FROM botline";
 $result2 = $conn->query($sql2);
 if ($result2->num_rows > 0) {
-    while($row2 = $result2->fetch_object()) {
+    while($row2 = $result2->fetch_assoc()) {
         echo "Name : " .$row2["Name"]."<br>";
         $gggg = $row2["Name"];
     }
