0ee379359a1a08e520686d5f81128b1bb41a20e9
ballangkub <45797334+ballangkub@users.noreply.github.com>
2018-12-24 18:27:31 +0700

    Create TEST.php

---
+++ TEST.php
@@ -0,0 +1,28 @@
+<?
+$servername = "us-cdbr-iron-east-01.cleardb.net";
+$username = "b1ab7271b69167";
+$password = "f300f19a";
+$dbname = "heroku_b577b61b9273cc5";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+mysqli_query($conn,"SET NAMES'utf8'");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else { 
+   // echo "Connection Success";
+}
+
+$text = 'TEST';
+$sql = "SELECT Name FROM botline WHERE Name = '$text' ";
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "UserID : " .$row["Name"].;
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+?>
