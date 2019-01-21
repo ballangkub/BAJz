2c8af8e649190e83a9e0200b5fed7558a6fbf74a
ballangkub <45797334+ballangkub@users.noreply.github.com>
2019-01-21 19:29:22 +0700

    Update sendmessage.php

--- sendmessage.php
+++ sendmessage.php
@@ -42,7 +42,7 @@ $access_token = '6Fkcia04Z6b5eNyPFvCTM98VKSofDCm3zr8tX1XrbPSdMBCCCDhNhxXiJTP3wIj
 
 $channelSecret = 'cb35ef400aeeb1531a9c836e5d3e72ed';
 
-$pushID = $push,$push1;
+$pushID = ($push,$push1);
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
