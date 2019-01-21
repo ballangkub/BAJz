551b9c5f82ea69e01a12f61a56b653ab6cb18fc0
ballangkub <45797334+ballangkub@users.noreply.github.com>
2019-01-21 19:33:12 +0700

    Update sendmessage.php

--- sendmessage.php
+++ sendmessage.php
@@ -17,13 +17,14 @@ if ($conn->connect_error) {
 $test1 = $_POST["txtname"];
 $test2 = $_POST["txtmessage"];
 
+$push1 = 'U065093edd69838903cc8aedf034df042';
+
 $sql = "SELECT UserID FROM botline WHERE Name = '$test1'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()){
         echo "UserID : " .$row["UserID"]."<br>";
         $push = $row["UserID"];
-        $push1 = 'U065093edd69838903cc8aedf034df042';
     }
 } else {
     echo "0 results";
@@ -42,7 +43,7 @@ $access_token = '6Fkcia04Z6b5eNyPFvCTM98VKSofDCm3zr8tX1XrbPSdMBCCCDhNhxXiJTP3wIj
 
 $channelSecret = 'cb35ef400aeeb1531a9c836e5d3e72ed';
 
-$pushID = '$push','$push1';
+$pushID = '$push1';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
