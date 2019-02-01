6131de47a271719633c743c54c98f2da628e54a5
ballangkub <45797334+ballangkub@users.noreply.github.com>
2019-02-01 20:27:16 +0700

    welcome

---
+++ welcome.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="iproject/public/css/stylesheet.css">
+
+    <title>iproject</title>
+  </head>
+  <body>
+  <form method="POST" action="welcome1.php">
+    <div class="container-fluid bg">
+        <div class="row">
+            <div class="col-4">                
+            </div>
+            <div class="col-4 text-center" style="margin-top:70px">
+                <img src="iproject/public/img/iproject.gif" width="500" height="250" >
+            </div>
+            <div class="col-4">                
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">                
+            </div>
+            <div class="col-4 text-center"  style="margin-top:60px">
+                <div class="form-group">
+                    <label for="Inputaccountid" class="float-left changefonts">Account ID</label>
+                    <input type="text" class="form-control" name="Inputaccountid" id="Inputaccountid" placeholder="Enter Account">
+                </div>
+				
+                <div class="form-group">
+                    <label for="InputPassword" class="float-left changefonts">Password</label>
+                    <input type="password" class="form-control" name="InputPassword" id="InputPassword" placeholder="Password">
+                </div>
+                    <input type="submit" value="Login" class="btn btn-success btn-lg float-right">
+                    <a href=""><input type="button" value="Register" class="btn btn-lg float-left"></a>
+                    <a href=""><input type="button" value="Forgot Password" class="btn btn-lg float-left"></a>
+                <!-- <button type="button" class="btn btn-success btn-lg" style="margin-right:20px">สมัครสมาชิก</button>
+                <button type="button" class="btn btn-success btn-lg" style="margin-left:20px">เข้าสู่ระบบ</button> -->
+            </div>
+            <div class="col-4">                
+            </div>
+        </div>
+    </div>
+	</form>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+  </body>
+</html>
