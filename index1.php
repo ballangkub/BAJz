09064c4266adac31dda54afe4ffa2a50794229ab
ballangkub <45797334+ballangkub@users.noreply.github.com>
2019-02-03 01:39:55 +0700

    Update index1.php

--- index1.php
+++ index1.php
@@ -29,7 +29,7 @@
             $text_to_segment = trim($_POST['text_to_segment']);
             echo '<hr/>';
             //echo '<b>ประโยคที่ต้องการตัดคือ: </b>' . $text_to_segment . '<br/>';
-            include("THSplitLib/segment.php");
+            include(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'THSplitLib/segment.php');
             $segment = new Segment();
             //echo '<hr/>';
             $result = $segment->get_segment_array($text_to_segment);
@@ -89,17 +89,4 @@
 
     </div>
 </body>
-<script type="text/javascript">
-
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-28746062-1']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-
-</script>
 </html>
